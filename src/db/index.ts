84dd26ba35f76db9c289b2b3203d5494a317e579
Sagar Dave <sagar@snappymob.com>
2022-02-14 18:22:03 +0800

    Add primsa client setup

---
+++ src/db/index.ts
@@ -0,0 +1,3 @@
+import { PrismaClient, Prisma, Product, User, Wishlist, Role } from '@prisma/client';
+export { PrismaClient, Prisma, Product, User, Wishlist, Role };
+export const prisma = new PrismaClient();
